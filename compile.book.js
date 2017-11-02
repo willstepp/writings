8d716251283304a6562e8e5d2592b3c296108b1a
Daniel Stepp <willstepp@gmail.com>
2017-11-02 12:52:15 -0400

    added helper scripts

---
+++ compile.book.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node 
+
+const glob = require('glob');
+const fs = require('fs');
+const path = require('path');
+
+let bookContent = '';
+
+if (process.argv.length <= 2) {
+  console.error('Usage: ' + __filename + ' <book-name>');
+  process.exit(-1);
+}
+
+// 1) read in book name from arg
+let bookName = process.argv[2];
+
+// 2) get list of all chapters from __dirname/<book name>/chapters
+let chapterSrc = __dirname + '/' + bookName + '/chapters/*.md';
+let chapters = glob.sync(chapterSrc);
+
+// 3) concatenate all chapter content into book
+for (let i = 0; i < chapters.length; i += 1) {
+  bookContent += fs.readFileSync(chapters[i]);
+}
+
+// 4) write book content to __dirname/<book name>.md
+fs.writeFileSync(__dirname + '/' + bookName + '/book.md', bookContent);
---
+++ count.book.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node 
+
+const glob = require('glob');
+const fs = require('fs');
+const path = require('path');
+const wordcount = require('wordcount');
+
+let bookWordCount = 0;
+
+if (process.argv.length <= 2) {
+  console.error('Usage: ' + __filename + ' <book-name>');
+  process.exit(-1);
+}
+
+// 1) read in book name from arg
+let bookName = process.argv[2];
+
+// 2) get list of all chapters from __dirname/<book name>/chapters
+let chapterSrc = __dirname + '/' + bookName + '/chapters/*.md';
+let chapters = glob.sync(chapterSrc);
+
+// 3) calculate all chapter word counts
+for (let i = 0; i < chapters.length; i += 1) {
+  bookWordCount += wordcount(fs.readFileSync(chapters[i]).toString());
+}
+
+console.log(bookName + ': ' + bookWordCount);
